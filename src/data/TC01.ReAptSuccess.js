6c9634bedf19809896e064387385931070aaf342
Bùi Thị Loan Anh <151595835+LoanAnhBui@users.noreply.github.com>
2023-11-21 11:28:46 +0700

    Add files via upload

---
+++ src/data/TC01.ReAptSuccess.js
@@ -0,0 +1,7 @@
+export const VALID_DAY_APT={
+    timeStart: '9',
+    subject: 'WebDriverIO',
+}
+
+export const ATTENDEE = 'User1'
+export const REGISTER = 'User3'
---
+++ src/data/TC02.ReAptUnsuccess.js
@@ -0,0 +1,7 @@
+export const VALID_DAY_APT={
+    timeStart: '10',
+    subject: 'Unsuccess Apt',
+}
+
+export const ATTENDEE = 'User2'
+export const REGISTER = 'User3'
